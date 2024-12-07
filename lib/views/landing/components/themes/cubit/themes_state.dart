3c4b850aa8c1ff961a9e6c89a2d4f4ef34112f80
hamza aboshhiwa <42789638+Hamza-Shewa@users.noreply.github.com>
2024-12-07 02:12:27 +0200

    clean up

--- lib/views/landing/components/themes/cubit/themes_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'themes_cubit.dart';
-
-@freezed
-class ThemesState with _$ThemesState {
-  const factory ThemesState.initial() = _Initial;
-  const factory ThemesState.loading() = _Loading;
-  const factory ThemesState.loaded() = _Loaded;
-  const factory ThemesState.error(String message) = _Error;
-}
